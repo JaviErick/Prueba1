9201f1a43a321adc6fe37906db6f1b86db84fbfe
JaviErick <118129322+JaviErick@users.noreply.github.com>
2023-09-30 11:42:23 -0500

    Add files via upload

---
+++ JavaApplication11/src/Calculadora.java
@@ -0,0 +1,24 @@
+
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        // Definir dos números
+        double numero1 = 10.5;
+        double numero2 = 5.2;
+
+        // Realizar algunas operaciones
+        double suma = numero1 + numero2;
+        double resta = numero1 - numero2;
+        double multiplicacion = numero1 * numero2;
+        double division = numero1 / numero2;
+
+        // Mostrar los resultados
+        System.out.println("Número 1: " + numero1);
+        System.out.println("Número 2: " + numero2);
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación: " + multiplicacion);
+        System.out.println("División: " + division);
+    }
+}
